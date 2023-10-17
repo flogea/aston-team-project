5967157660caba4cb9a9d2c2eea4a25bd566e90c
Edward <112203542+SenSeTioN@users.noreply.github.com>
2023-10-17 20:43:16 +0300

    init: init app (#3)
    * init: init app
    
    * feat: add file structure

---
+++ webpack.config.ts
@@ -0,0 +1,60 @@
+const path = require('path')
+const env = require('dotenv')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const BuildConfig = {
+  path: {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    dist: path.resolve(__dirname, 'dist'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src'),
+  },
+}
+
+const getRules = () => {
+  return [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(css|scss)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+    {
+      test: /\.(png|jpg|gif|jpeg)$/,
+      use: ['file-loader'],
+    },
+    {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    },
+  ]
+}
+
+const getPlugins = () => {
+  return [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: BuildConfig.path.html,
+    }),
+  ]
+}
+
+module.exports = {
+  entry: BuildConfig.path.entry,
+  devtool: 'inline-source-map',
+  output: {
+    path: BuildConfig.path.dist,
+    filename: '[name].[hash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
+  plugins: getPlugins(),
+  module: {
+    rules: getRules(),
+  },
+}
