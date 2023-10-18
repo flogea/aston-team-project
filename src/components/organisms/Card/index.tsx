0ef30847c67c80f230b4f34410ee8f74af9b1129
flogea <katua.logvinova@gmail.com>
2023-10-18 18:57:34 +0300

    Card: like & title on card

--- src/components/organisms/Card/index.tsx
+++ src/components/organisms/Card/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { CardPicture } from '../../molecules'
 
 import './Card.scss'
 
-function Card({ img, title = 'Title', author = 'Author' }) {
+import fav from '../../../assets/icons/fav.png'
+import favFilled from '../../../assets/icons/fav-filled.png'
+
+function Card({ id, urls, description = 'title', liked_by_user }) {
+  const [isLiked, setIsLiked] = React.useState(liked_by_user)
+  const handleLikePost = () => {
+    setIsLiked(() => !isLiked)
+  }
+
   return (
     <div className='card'>
-      <CardPicture img={img} />
+      <div className='fav'>
+        <button className='favBtn' onClick={handleLikePost}>
+          <img src={isLiked ? favFilled : fav} alt='' />
+        </button>
+      </div>
+      <div className='cardPicture'>
+        <img src={urls.regular} alt='image' />
+      </div>
       <div className='cardBottom'>
-        <div className='cardTitle'>{title}</div>
-        <div className='cardAuthor'>
-          <Link to='/'>{author}</Link>
-        </div>
+        <div className='cardTitle'>{description}</div>
       </div>
     </div>
   )
